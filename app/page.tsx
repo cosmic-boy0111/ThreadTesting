90ba36dae7edc637fdfbeed8ddf96dfcd6734e37
cosmic-boy0111 <gauravbhagat2040@gmail.com>
2023-12-15 21:24:42 +0530

    chnages

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
 'use client'
+
+import Link from "next/link";
+
 export default function Home() {
 
-  const handleDelete = () => {
-    console.log('btn click');
-    
-    window.confirm(' you really want to delete')
-  }
 
   return (
     <div >
       <button
-        onClick={handleDelete}
+        
       >
-        <h1>
-          Delete
-        </h1>
+        <Link href={'/test'}>
+          Go to Test
+        </Link>
       </button>
     </div>
   )
